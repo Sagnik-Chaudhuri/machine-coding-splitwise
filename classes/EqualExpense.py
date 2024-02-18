e88b9afa2b7897c32fa8cdcf7dfe0fa6afc4f330
Sagnik-Chaudhuri <csagnik.chaudhuri@gmail.com>
2024-02-18 20:38:54 +0530

    init commit

--- classes/EqualExpense.py
+++
@@ -1,8 +0,0 @@
-from config import EQUAL_EXPENSE_TYPE_NAME
-from classes.Expense import Expense
-
-
-class EqualExpense(Expense):
-    def __init__(self, amount, paid_by, splits) -> None:
-        super().__init__(amount, paid_by, splits)
-        self.type = EQUAL_EXPENSE_TYPE_NAME
--- classes/ExactSplit.py
+++
@@ -1,8 +0,0 @@
-from config import EXACT_EXPENSE_TYPE_NAME
-from classes.Split import Split
-
-
-class ExactSplit(Split):
-    def __init__(self, user_id, amount) -> None:
-        super().__init__(user_id, amount)
-        self.type = EXACT_EXPENSE_TYPE_NAME
